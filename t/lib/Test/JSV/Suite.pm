ef57017421e1e6a42426f178d9a0d9bded4df443
Toru Yamaguchi <zigorou@cpan.org>
2013-06-14 00:21:21 +0900

    add test suite and test library

---
+++ t/lib/Test/JSV/Suite.pm
@@ -0,0 +1,74 @@
+package Test::JSV::Suite;
+
+use strict;
+use warnings;
+
+use Carp;
+use File::Spec;
+use FindBin;
+use JSON;
+use Test::More;
+
+sub run {
+    my ($class, %opts) = @_;
+
+    %opts = (
+        base_dir => File::Spec->catdir($FindBin::Bin, "t/suite/tests"),
+        version  => "draft4",
+        suite    => "type.json",
+        cb       => sub {
+            my ($schema, $data) = @_;
+            my $rv = 1;
+            return $rv;
+        },
+        verbose => 0,
+        %opts
+    );
+
+    my $self = bless \%opts => $class;
+    my $test_suite = $self->load_test_suite;
+
+    for my $test_cases (@$test_suite) {
+        $self->run_test_cases($test_cases);
+    }
+
+    done_testing;
+}
+
+sub run_test_cases {
+    my ($self, $test_cases) = @_;
+
+    my ($desc, $schema, $tests) = @$test_cases{qw/description schema tests/};
+
+    subtest $desc => sub {
+        for my $test_case (@$tests) {
+            $self->run_test_case($schema, $test_case);
+        }
+    };
+}
+
+sub run_test_case {
+    my ($self, $schema, $test_case) = @_;
+    my ($desc, $data, $expect) = @$test_case{qw/description data valid/};
+
+    is(
+        $self->{cb}->($schema, $data),
+        !!$expect,
+        $desc,
+    );
+}
+
+sub load_test_suite {
+    my $self = shift;
+    my $test_suite_file = File::Spec->catfile($self->{base_dir}, $self->{version}, $self->{suite});
+    unless (-f $test_suite_file) {
+        croak sprintf("Not exists test suite (base_dir: %s, version: %s, suite: %s)", @$self{qw/base_dir version suite/});
+    }
+
+    open(my $fh, "<", $test_suite_file) or croak $!;
+    my $test_suite = decode_json(do { local $/; <$fh> });
+    close $fh;
+    return $test_suite;
+}
+
+1;
