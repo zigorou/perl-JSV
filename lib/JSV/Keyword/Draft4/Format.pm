38edac10a39d90b02a5eeb9aa6dd64dc15c57868
Tomohiro Mitsumune <tomohiro.mitsumune@dena.com>
2013-11-22 21:08:41 +0900

    Fix JSV::Context

--- lib/JSV/Keyword/Draft4/Format.pm
+++ lib/JSV/Keyword/Draft4/Format.pm
@@ -9,55 +9,45 @@ use Data::Validate::IP qw(is_ipv4 is_ipv6);
 use Data::Validate::URI qw(is_uri);
 use Email::Valid::Loose;
 use JSV::Keyword qw(:constants);
-use JSV::Exception;
 
-sub instance_type { INSTANCE_TYPE_STRING(); }
-sub keyword { "format" }
-sub keyword_priority { 10; }
+sub instance_type() { INSTANCE_TYPE_STRING(); }
+sub keyword() { "format" }
+sub keyword_priority() { 10; }
 
 sub validate {
-    my ($class, $validator, $schema, $instance, $opts) = @_;
-    return 1 unless ( $class->has_keyword($schema) );
+    my ($class, $context, $schema, $instance) = @_;
 
-    $opts ||= {};
-    $class->initialize_args($schema, $instance, $opts);
+    my $format = $class->keyword_value($schema);
 
-    unless ($opts->{type} eq "string") {
-        return 1;
-    }
-
-    my $keyword_value = $class->keyword_value($schema);
-
-    if ($keyword_value eq 'date-time') {
+    if ($format eq 'date-time') {
         # RFC3339
         unless ($instance =~ /\A\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d+)?(Z|[+-]\d{2}:\d{2})/) {
-            JSV::Exception->throw("format isn`t date-time", $opts);
+            $context->log_error(sprintf("format isn`t date-time: %s", $instance));
         }
-    } elsif ($keyword_value eq 'uri') {
+    } elsif ($format eq 'uri') {
         unless (is_uri($instance)) {
-            JSV::Exception->throw("format isn`t uri", $opts);
+            $context->log_error(sprintf("format isn`t uri: %s", $instance));
         }
-
-    } elsif ($keyword_value eq 'email') {
+    } elsif ($format eq 'email') {
         # TODO: enable change to Email::Valid or Email::Valid::Loose
         unless (Email::Valid::Loose->address($instance)) {
-            JSV::Exception->throw("format isn`t email", $opts);
+            $context->log_error(sprintf("format isn`t email: %s", $instance));
         }
-    } elsif ($keyword_value eq 'ipv4') {
+    } elsif ($format eq 'ipv4') {
         unless (is_ipv4($instance)) {
-            JSV::Exception->throw("format isn`t ipv4", $opts);
+            $context->log_error(sprintf("format isn`t ipv4: %s", $instance));
         }
-    } elsif ($keyword_value eq 'ipv6') {
+    } elsif ($format eq 'ipv6') {
         unless (is_ipv6($instance)) {
-            JSV::Exception->throw("format isn`t ipv6", $opts);
+            $context->log_error(sprintf("format isn`t ipv6: %s", $instance));
         }
-    } elsif ($keyword_value eq 'hostname') {
+    } elsif ($format eq 'hostname') {
         unless (is_domain($instance)) {
-            JSV::Exception->throw("format isn`t hostname", $opts);
+            $context->log_error(sprintf("format isn`t hostname: %s", $instance));
         }
     } else {
-        JSV::Exception->throw("disable format", $opts);
+        $context->log_error(sprintf("unknown format: format = %s, value = %s", $format, $instance));
     }
-
-    return 1;
 }
+
+1;
