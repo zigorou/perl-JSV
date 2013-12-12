52ebbecae6610885035f049d787b1e555d6b82c6
zigorou <zigorou@cpan.org>
2013-12-13 01:14:53 +0900

    fixed string instance passed numeric type constraint bug

--- lib/JSV/Keyword/Draft4/Type.pm
+++ lib/JSV/Keyword/Draft4/Type.pm
@@ -7,7 +7,7 @@ use parent qw(JSV::Keyword);
 use B;
 use JSON;
 use List::Util qw(first);
-use Scalar::Util qw(blessed);
+use Scalar::Util qw(blessed looks_like_number);
 
 use JSV::Keyword qw(:constants);
 use JSV::Util::Type qw(detect_instance_type);
@@ -22,26 +22,30 @@ sub validate {
     my $keyword_value = $class->keyword_value($schema);
 
     if (ref $keyword_value eq "ARRAY") {
-        unless ( first { $class->validate_singular_type( $context, $_, $context->current_type ) } @$keyword_value ) {
+        unless ( first { $class->validate_singular_type( $context, $_, $context->current_type, $instance ) } @$keyword_value ) {
             $context->log_error("instance type doesn't match schema type list");
         }
     }
     else {
-        unless ($class->validate_singular_type( $context, $keyword_value, $context->current_type )) {
+        unless ($class->validate_singular_type( $context, $keyword_value, $context->current_type, $instance )) {
             $context->log_error("instance type doesn't match schema type");
         }
     }
 }
 
 sub validate_singular_type {
-    my ($class, $context, $schema_type, $given_type) = @_;
+    my ($class, $context, $schema_type, $given_type, $instance) = @_;
 
     if ( $schema_type eq $given_type || ( $schema_type eq "number" && $given_type eq "integer") ) {
         return 1;
     }
     else {
-        if ($context->loose_type && ( ( $schema_type eq "number" || $schema_type eq "integer" ) && $given_type eq "string" )) {
-            return 1;
+        # if ($context->loose_type && ( ( $schema_type eq "number" || $schema_type eq "integer" ) && $given_type eq "string" )) {
+        #    return 1;
+        #}
+        if ($context->loose_type && $given_type eq "string" && looks_like_number($instance)) {
+            return 1 if ($schema_type eq "number");
+            return 1 if ($schema_type eq "integer" && $instance =~ m/^(?:[+-])?[1-9]?\d+$/);
         }
 
         return 0;
