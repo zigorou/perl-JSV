43aa732b99c3008f75e4de4ff02f0e99ce9afd9d
takkyuuplayer <takkyuuplayer@gmail.com>
2016-05-24 21:15:49 +0900

    validate with loose_type for enum

--- lib/JSV/Keyword/Draft4/Enum.pm
+++ lib/JSV/Keyword/Draft4/Enum.pm
@@ -6,7 +6,7 @@ use parent qw(JSV::Keyword);
 
 use JSV::Keyword qw(:constants);
 use JSON;
-use List::MoreUtils qw(firstidx);
+use List::MoreUtils qw(any firstidx);
 
 sub instance_type() { INSTANCE_TYPE_ANY(); }
 sub keyword() { "enum" }
@@ -16,10 +16,18 @@ sub validate {
     my ($class, $context, $schema, $instance) = @_;
 
     my $enum = $class->keyword_value($schema);
-    my $instance_as_json = $context->json->encode($instance);
-    my $matched_idx = firstidx { $instance_as_json eq $context->json->encode($_); } @$enum;
 
-    if ($matched_idx == -1) {
+    my $is_valid = $context->loose_type
+        ? do {
+        any { $instance == $_; } @$enum;
+        }
+        : do {
+        my $instance_as_json = $context->json->encode($instance);
+        my $matched_idx = firstidx { $instance_as_json eq $context->json->encode($_); } @$enum;
+        $matched_idx != -1;
+        };
+
+    unless ($is_valid) {
         $context->log_error("The instance value does not be included in the enum list");
     }
 }
